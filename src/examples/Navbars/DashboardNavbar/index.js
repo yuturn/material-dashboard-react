3b51ebd880f625ad9072aada4f7a0f48e80774a7
yuturn <brave870307@gmail.com>
2023-06-12 02:20:13 +0800

    backup page

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -13,32 +13,33 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState, useEffect } from "react";
+// import { useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 // react-router components
 import { useLocation } from "react-router-dom";
 // import { useLocation, Link } from "react-router-dom";
 
 // prop-types is a library for typechecking of props.
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 // @material-ui core components
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+// import Toolbar from "@mui/material/Toolbar";
 // import IconButton from "@mui/material/IconButton";
 // import Menu from "@mui/material/Menu";
 // import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
+// import MDBox from "components/MDBox";
 // import MDInput from "components/MDInput";
 
 // Material Dashboard 2 React example components
-import Breadcrumbs from "examples/Breadcrumbs";
+// import Breadcrumbs from "examples/Breadcrumbs";
 // import NotificationItem from "examples/Items/NotificationItem";
 
 // Custom styles for DashboardNavbar
-import { navbar, navbarContainer, navbarRow } from "examples/Navbars/DashboardNavbar/styles";
+// import { navbar } from "examples/Navbars/DashboardNavbar/styles";
 
 // import {
 //   navbar,
@@ -49,7 +50,7 @@ import { navbar, navbarContainer, navbarRow } from "examples/Navbars/DashboardNa
 // } from "examples/Navbars/DashboardNavbar/styles";
 
 // Material Dashboard 2 React context
-import { useMaterialUIController, setTransparentNavbar } from "context";
+// import { useMaterialUIController, setTransparentNavbar } from "context";
 
 // import {
 //   useMaterialUIController,
@@ -58,39 +59,39 @@ import { useMaterialUIController, setTransparentNavbar } from "context";
 //   setOpenConfigurator,
 // } from "context";
 
-function DashboardNavbar({ absolute, light, isMini }) {
-  const [navbarType, setNavbarType] = useState();
-  const [controller, dispatch] = useMaterialUIController();
-  const { transparentNavbar, fixedNavbar, darkMode } = controller;
+// import Grid from "@mui/material/Grid";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function DashboardNavbar() {
+  // const [navbarType, setNavbarType] = useState();
+  // const [controller, dispatch] = useMaterialUIController();
+  // const { transparentNavbar, fixedNavbar, darkMode } = controller;
   // const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator, darkMode } = controller;
   // const [openMenu, setOpenMenu] = useState(false);
   const route = useLocation().pathname.split("/").slice(1);
+  const username = "使用者姓名";
 
-  useEffect(() => {
-    // Setting the navbar type
-    if (fixedNavbar) {
-      setNavbarType("sticky");
-    } else {
-      setNavbarType("static");
-    }
+  // useEffect(() => {
+  //   Setting the navbar type
+  //   if (fixedNavbar) {
+  //     setNavbarType("sticky");
+  //   } else {
+  //     setNavbarType("static");
+  //   }
 
-    // A function that sets the transparent state of the navbar.
-    function handleTransparentNavbar() {
-      setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
-    }
+  //   A function that sets the transparent state of the navbar.
+  //   function handleTransparentNavbar() {
+  //     setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
+  //   }
 
-    /** 
-     The event listener that's calling the handleTransparentNavbar function when 
-     scrolling the window.
-    */
-    window.addEventListener("scroll", handleTransparentNavbar);
+  //   Call the handleTransparentNavbar function to set the state with the initial value.
+  //   handleTransparentNavbar();
 
-    // Call the handleTransparentNavbar function to set the state with the initial value.
-    handleTransparentNavbar();
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("scroll", handleTransparentNavbar);
-  }, [dispatch, fixedNavbar]);
+  //   Remove event listener on cleanup
+  //   return () => window.removeEventListener("scroll", handleTransparentNavbar);
+  // }, [dispatch, fixedNavbar]);
 
   // const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
   // const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
@@ -130,64 +131,18 @@ function DashboardNavbar({ absolute, light, isMini }) {
   // });
 
   return (
-    <AppBar
-      position={absolute ? "absolute" : navbarType}
-      color="inherit"
-      sx={(theme) => navbar(theme, { transparentNavbar, absolute, light, darkMode })}
-    >
-      <Toolbar sx={(theme) => navbarContainer(theme)}>
-        <MDBox color="inherit" mb={{ xs: 1, md: 0 }} sx={(theme) => navbarRow(theme, { isMini })}>
-          <Breadcrumbs icon="home" title={route[route.length - 1]} route={route} light={light} />
-        </MDBox>
-        {isMini ? null : (
-          <MDBox sx={(theme) => navbarRow(theme, { isMini })}>
-            {/* <MDBox pr={1}>
-              <MDInput label="Search here" />
-            </MDBox>
-            <MDBox color={light ? "white" : "inherit"}>
-              <Link to="/authentication/sign-in/basic">
-                <IconButton sx={navbarIconButton} size="small" disableRipple>
-                  <Icon sx={iconsStyle}>account_circle</Icon>
-                </IconButton>
-              </Link>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarMobileMenu}
-                onClick={handleMiniSidenav}
-              >
-                <Icon sx={iconsStyle} fontSize="medium">
-                  {miniSidenav ? "menu_open" : "menu"}
-                </Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarIconButton}
-                onClick={handleConfiguratorOpen}
-              >
-                <Icon sx={iconsStyle}>settings</Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon sx={iconsStyle}>notifications</Icon>
-              </IconButton>
-              {renderMenu()}
-            </MDBox> */}
-          </MDBox>
-        )}
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 3 }}>
+            {route}
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexDirection: "row-reverse" }}>
+            {username}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
@@ -195,14 +150,12 @@ function DashboardNavbar({ absolute, light, isMini }) {
 DashboardNavbar.defaultProps = {
   absolute: false,
   light: false,
-  isMini: false,
 };
 
 // Typechecking props for the DashboardNavbar
-DashboardNavbar.propTypes = {
-  absolute: PropTypes.bool,
-  light: PropTypes.bool,
-  isMini: PropTypes.bool,
-};
+// DashboardNavbar.propTypes = {
+//   absolute: PropTypes.bool,
+//   light: PropTypes.bool,
+// };
 
 export default DashboardNavbar;
--- src/layouts/rtl/index.js
+++ src/layouts/rtl/index.js
@@ -1,25 +1,10 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // import { useEffect } from "react";
 
 // @mui material components
 // import Grid from "@mui/material/Grid";
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
+// import MDBox from "components/MDBox";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -40,136 +25,239 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 // Material Dashboard 2 React contexts
 // import { useMaterialUIController, setDirection } from "context";
 
+import Grid from "@mui/material/Grid";
+// import Box from "@mui/material/Box";
+import {
+  Card,
+  ThemeProvider,
+  createTheme,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const completedBackupTime = "2023/06/11";
+const differentBackupTime = "2023/06/12";
+const manualBackupTime = "2023/06/12";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#5048E5",
+      light: "#828DF8",
+      dark: "#7582EB",
+      contrastText: "#FFFFFF",
+    },
+    background: {
+      default: "#1a1e2b",
+      paper: "#1a1e2b",
+    },
+    text: {
+      primary: "#fff",
+    },
+  },
+});
+
 function RTL() {
-  // const [, dispatch] = useMaterialUIController();
-  // const { sales, tasks } = reportsLineChartData;
-
-  // Changing the direction to rtl
-  // useEffect(() => {
-  //   setDirection(dispatch, "rtl");
-
-  //   return () => setDirection(dispatch, "ltr");
-  // }, []);
-
   return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <MDBox py={3}>
+    <ThemeProvider theme={darkTheme}>
+      <DashboardLayout>
+        <DashboardNavbar />
+        <Card>
+          <CardHeader title="前次備份時間" />
+          <Divider sx={{ borderBottomWidth: 3 }} />
+          <CardContent>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                完整備份:{completedBackupTime}
+              </Grid>
+              <Grid item xs={12}>
+                差異備份:{differentBackupTime}
+              </Grid>
+              <Grid item xs={12}>
+                手動完整備份:{manualBackupTime}
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader title="設定" />
+          <Divider sx={{ borderBottomWidth: 3 }} />
+          <CardContent>
+            <Grid container spacing={4}>
+              <Grid item xs={12}>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  完整備份時間
+                  <TextField
+                    label="請輸入備份時間,152359"
+                    margin="normal"
+                    name="fullbackup"
+                    id="fullbackup"
+                    variant="outlined"
+                    size="small"
+                  />
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  差異備份時間
+                  <TextField
+                    label="請輸入備份時間,152359"
+                    margin="normal"
+                    name="diffbackup"
+                    id="diffbackup"
+                    variant="outlined"
+                    size="small"
+                  />
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 3 }}>
+                  備份目的地1:
+                  <TextField
+                    label="請輸入備份時間,152359"
+                    margin="normal"
+                    name="ip1"
+                    id="ip1"
+                    variant="outlined"
+                    size="small"
+                  />
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 3 }}>
+                  備份目的地2:
+                  <TextField
+                    label="請輸入備份時間,152359"
+                    margin="normal"
+                    name="ip2"
+                    id="ip2"
+                    variant="outlined"
+                    size="small"
+                  />
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
         {/* <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="dark"
-                icon="weekend"
-                title="أموال اليوم"
-                count={281}
-                percentage={{
-                  color: "success",
-                  amount: "+55%",
-                  label: "من الأسبوع الماضي",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                icon="leaderboard"
-                title="مستخدمو اليوم"
-                count="2,300"
-                percentage={{
-                  color: "success",
-                  amount: "+3%",
-                  label: "من الأسبوع الماضي",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="success"
-                icon="store"
-                title="عملاء جدد"
-                count="34k"
-                percentage={{
-                  color: "success",
-                  amount: "+1%",
-                  label: "من الشهر الماضي",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="primary"
-                icon="person_add"
-                title="مبيعات"
-                count="+91"
-                percentage={{
-                  color: "success",
-                  amount: "",
-                  label: "مقارنة بيوم أمس",
-                }}
-              />
-            </MDBox>
-          </Grid>
-        </Grid>
-        <MDBox mt={4.5}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsBarChart
-                  color="info"
-                  title="مشاهدات الموقع"
-                  description="آخر أداء للحملة"
-                  date="الحملة أرسلت قبل يومين"
-                  chart={reportsBarChartData}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
-                  color="success"
-                  title="المبيعات اليومية"
-                  description={
-                    <>
-                      (<strong>+15%</strong>) زيادة في مبيعات اليوم..
-                    </>
-                  }
-                  date="تم التحديث منذ 4 دقائق"
-                  chart={sales}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
+            <Grid item xs={12} md={6} lg={3}>
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
                   color="dark"
-                  title="المهام المكتملة"
-                  description="آخر أداء للحملة"
-                  date="تم تحديثه للتو"
-                  chart={tasks}
+                  icon="weekend"
+                  title="أموال اليوم"
+                  count={281}
+                  percentage={{
+                    color: "success",
+                    amount: "+55%",
+                    label: "من الأسبوع الماضي",
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={3}>
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  icon="leaderboard"
+                  title="مستخدمو اليوم"
+                  count="2,300"
+                  percentage={{
+                    color: "success",
+                    amount: "+3%",
+                    label: "من الأسبوع الماضي",
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={3}>
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  color="success"
+                  icon="store"
+                  title="عملاء جدد"
+                  count="34k"
+                  percentage={{
+                    color: "success",
+                    amount: "+1%",
+                    label: "من الشهر الماضي",
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={3}>
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  color="primary"
+                  icon="person_add"
+                  title="مبيعات"
+                  count="+91"
+                  percentage={{
+                    color: "success",
+                    amount: "",
+                    label: "مقارنة بيوم أمس",
+                  }}
                 />
               </MDBox>
             </Grid>
           </Grid>
-        </MDBox>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              <Projects />
+          <MDBox mt={4.5}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6} lg={4}>
+                <MDBox mb={3}>
+                  <ReportsBarChart
+                    color="info"
+                    title="مشاهدات الموقع"
+                    description="آخر أداء للحملة"
+                    date="الحملة أرسلت قبل يومين"
+                    chart={reportsBarChartData}
+                  />
+                </MDBox>
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <MDBox mb={3}>
+                  <ReportsLineChart
+                    color="success"
+                    title="المبيعات اليومية"
+                    description={
+                      <>
+                        (<strong>+15%</strong>) زيادة في مبيعات اليوم..
+                      </>
+                    }
+                    date="تم التحديث منذ 4 دقائق"
+                    chart={sales}
+                  />
+                </MDBox>
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <MDBox mb={3}>
+                  <ReportsLineChart
+                    color="dark"
+                    title="المهام المكتملة"
+                    description="آخر أداء للحملة"
+                    date="تم تحديثه للتو"
+                    chart={tasks}
+                  />
+                </MDBox>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <OrdersOverview />
+          </MDBox>
+          <MDBox>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6} lg={8}>
+                <Projects />
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <OrdersOverview />
+              </Grid>
             </Grid>
-          </Grid>
-        </MDBox> */}
-      </MDBox>
-      {/* <Footer /> */}
-    </DashboardLayout>
+          </MDBox> */}
+        {/* <Footer /> */}
+      </DashboardLayout>
+    </ThemeProvider>
   );
 }
 
